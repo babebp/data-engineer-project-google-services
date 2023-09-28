aa83799a6d35ac97142f5abdad3f78d34d041238
babebp <tpacharawut@hotmail.com>
2023-09-28 19:39:51 +0700

    store data into bucket with bash and python

---
+++ store_data/download.sh
@@ -0,0 +1,3 @@
+# download from bucket
+# you can add -m for activate multi-procession or -r for download folder
+gsutil cp gs://[BUCKET_NAME]/[SOURCE_FILE_NAME] [DESTINATION_FILE_NAME]
---
+++ store_data/upload_blob.sh
@@ -0,0 +1,3 @@
+# upload file into bucket
+# you can add -m for activate multi-procession or -r for upload folder
+gsutil cp [FILE_NAME] gs://[BUCKET_NAME]
